4139c47aacb5cb498bc67cfd97ca724cdbc6d605
Nunah31 <nunah31@gmail.com>
2026-06-11 15:45:40 +0300

    Fix duplicates bar: always show bulk select/delete buttons in two rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'um-contacts-v12';
+const CACHE = 'um-contacts-v13';
 const BASE = self.registration.scope;
 const FILES = [BASE, BASE + 'index.html', BASE + 'icon.png', BASE + 'contacts_data.json'];
 
