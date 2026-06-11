14d6a346329eb2e2c8ae9f7551d59345b7be3ed3
Nunah31 <nunah31@gmail.com>
2026-06-11 15:56:19 +0300

    Reminder banner: show as small centered pill with X button instead of full-width bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'um-contacts-v13';
+const CACHE = 'um-contacts-v14';
 const BASE = self.registration.scope;
 const FILES = [BASE, BASE + 'index.html', BASE + 'icon.png', BASE + 'contacts_data.json'];
 
